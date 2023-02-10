571e1f0893348e93400ec96bb18a65a5e255ec60
Maxim Shteinberg <maxim518@mail.ru>
2023-02-10 13:47:23 +0300

    Sem3Dz

---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,34 @@
+﻿// // Задача 21
+
+// // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние 
+// // между ними в 3D пространстве.
+
+// // A (3,6,8); B (2,1,-7), -> 15.84
+
+// // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+double Square(int x, int y)
+{
+    return Math.Pow((y - x), 2);
+}
+
+
+int X1 = InputNumber("Введите координату X1: ");
+int Y1 = InputNumber("Введите координату Y1: ");
+int Z1 = InputNumber("Введите координату Z1: ");
+
+int X2 = InputNumber("Введите координату X2: ");
+int Y2 = InputNumber("Введите координату Y2: ");
+int Z2 = InputNumber("Введите координату Z2: ");
+
+System.Console.WriteLine(Math.Round(Math.Pow((Square(X1, X2) + Square(Y1, Y2) + Square(Z1, Z2)), 0.5), 2));
+
+
+// // System.Console.WriteLine(Math.Round(Math.Pow(Math.Pow((X2-X1),2)+Math.Pow((Y2-Y1),2),0.5),2));
---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// System.Console.Write("Введите число N: ");
+// int n1 = Convert.ToInt32(Console.ReadLine());
+
+// int index=1;
+// while (index <= n1 ) 
+// {
+//     double result = Math.Pow(index,3);
+//     System.Console.WriteLine($"Куб числа {index} = {result}");
+//     index++;
+
+// }
+
+
+
+
+
+int GetNumber(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void GetResult(int n1)
+{
+    int index = 1;
+    while (index <= n1)
+    {
+        double result = Math.Pow(index, 3);
+        System.Console.WriteLine($"Куб числа {index} = {result}");
+        index++;
+
+    }
+}
+
+
+int N = GetNumber("Введите число N: ");
+GetResult(N);
